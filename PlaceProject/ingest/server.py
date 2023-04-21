39879f611454280ce7897aa1420b1c1a246fc6ff
Adam Cox <mr.adamcox@gmail.com>
2023-04-21 14:00:29 -0500

    Facilitate non-Docker development

--- PlaceProject/ingest/server.py
+++ PlaceProject/ingest/server.py
@@ -1,10 +1,19 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from controller.ingest import ingest
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(ingest)
 
-app.run("0.0.0.0", 80)
+mode = os.getenv("MODE", "prod")
+
+if mode == "prod":
+    app.run("0.0.0.0", 80)
+else:
+    app.run(debug=True, host='127.0.0.1', port=5000)
