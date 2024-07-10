83531a7f5a91aecf0b525c9d0fd0768223f1fdf9
Adam Cox <mr.adamcox@gmail.com>
2024-07-10 22:04:52 +0000

    properly read .env values
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
-set -a # automatically export all variables
-source .env
-set +a
-
 THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 PIDFILE=$THIS_DIR/.pid
 LOGFILE=$THIS_DIR/.log
+
+set -a # automatically export all variables
+source $THIS_DIR/../.env
+set +a
 
 $THIS_DIR/../env/bin/gunicorn -w 4 manager.app:app \
     --daemon \
