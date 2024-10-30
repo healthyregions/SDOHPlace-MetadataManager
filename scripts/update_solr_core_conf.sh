71b0a5ff7f032c382c9c7051ccd8d26442ed79ae
Adam Cox <mr.adamcox@gmail.com>
2024-10-30 15:23:22 +0000

    add timestamp to core backup during update conf process

--- scripts/update_solr_core_conf.sh
+++ scripts/update_solr_core_conf.sh
@@ -2,11 +2,8 @@
 
 THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-sudo su - solr -c "/opt/solr/bin/solr create -c $1 -n data_driven_schema_configs"
-
-sudo mv /var/solr/data/$1/conf /var/solr/data/$1/conf-backup
+sudo mv /var/solr/data/$1/conf /var/solr/data/$1/conf-backup-$(date +"%Y%m%d%H%M")
 sudo cp $THIS_DIR/../solr/conf /var/solr/data/$1/ -r
 
 sudo service solr restart
 
-$THIS_DIR/../env/bin/flask index --clean
