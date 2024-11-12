0b68dece129fd85d7e54676314c8489b87b94604
Adam Cox <mr.adamcox@gmail.com>
2024-11-12 00:10:29 +0000

    fix script names

--- scripts/restart_flask.sh
+++ scripts/restart_flask.sh
@@ -3,8 +3,8 @@
 THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 echo "stopping..."
-source $THIS_DIR/stop.sh
+source $THIS_DIR/stop_flask.sh
 sleep 1
 echo "starting..."
-source $THIS_DIR/start.sh
+source $THIS_DIR/start_flask.sh
 echo "done."
