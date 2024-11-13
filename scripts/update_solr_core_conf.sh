5868a4f8d6fb467c58e44437239446cf0e76a444
Adam Cox <mr.adamcox@gmail.com>
2024-11-13 20:56:10 +0000

    fix missing core name replacement in update core script

--- scripts/update_solr_core_conf.sh
+++ scripts/update_solr_core_conf.sh
@@ -5,5 +5,7 @@ THIS_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 sudo mv /var/solr/data/$1/conf /var/solr/data/$1/conf-backup-$(date +"%Y%m%d%H%M")
 sudo cp $THIS_DIR/../solr/conf /var/solr/data/$1/ -r
 
+sudo sed -i -e "s/{{SOLR_CORE}}/$1/g" /var/solr/data/$1/conf/solrconfig.xml
+
 sudo service solr restart
 
